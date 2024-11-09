942656ecbe1d97b9646bacf039496b0d0c7344b4
Deepak Pandey <deepakpandey392003@gmail.com>
2024-11-09 18:34:21 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,4 @@
+object types / data types = 
+
+1.numbers  = 1234, 3.14, 3+4j , 0b111, decimal(), fraction()..
+2.strings = 
---
+++ hello_chai.py
@@ -0,0 +1,11 @@
+print("My first Python code")
+print("My name is deepak and i love shree , i love my sis , my dad , my mom and myself too")
+
+# defining a function
+
+def chai(n):
+    print(n)
+
+chai(4)
+
+print("Hello kRISHNA")
